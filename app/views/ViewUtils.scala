7a6b9127b3a67b0a7498d9b6abbdb82fe94965af
Alexey Kabelitskiy <alexey.kabelitsky@gmail.com>
2014-04-22 23:25:22 +0400

    small cleanup

--- app/views/ViewUtils.scala
+++ app/views/ViewUtils.scala
@@ -5,8 +5,22 @@ import controllers._
 /**
  */
 object ViewUtils {
+  /**
+   * Find resource in WebJars by its path
+   * @param path
+   * @return
+   */
+  def atWebJars(path: String): String = {
+    routes.WebJarAssets.at(WebJarAssets.locate(path)).toString()
+  }
+
+  /**
+   * Find resource in WebJars by its path and strip .js. Useful for RequireJS configuration
+   * @param path
+   * @return
+   */
   def atWebJarsJS(path: String):String = {
-    val resolvedPath = routes.WebJarAssets.at(WebJarAssets.locate(path)).toString()
+    val resolvedPath = atWebJars(path)
     if(resolvedPath.endsWith(".js"))
       resolvedPath.substring(0,resolvedPath.length - 3)
     else
