3d970c195c2d221e04e1548f2b51c998cbe60ff5
Alexey Kabelitskiy <alexey.kabelytskyi@medhost.com>
2014-07-14 17:29:15 +0300

    embellishment

--- app/social/views/RequirePatch.scala
+++ app/social/views/RequirePatch.scala
@@ -46,44 +46,46 @@ object RequirePatch {
           webjarConfigsString.append("\n").append(getWebJarConfig(info));
         }
         // remove the trailing ", "
-        webjarsVersionsString.delete(webjarsVersionsString.length - 2, webjarsVersionsString.length)
+        if(webjarsVersionsString.length > 1)
+          webjarsVersionsString.delete(webjarsVersionsString.length - 2, webjarsVersionsString.length)
 
         // assemble the JavaScript
-        // todo: could use a templating language but that would add a dependency
-        val javaScript = "var webjars = {\n" +
-          "    versions: { " + webjarsVersionsString.toString + " },\n" +
-          "    path: function(webjarid, path) {\n" +
-          "        return '" + webjarUrlPrefix + "' + webjarid + '/' + webjars.versions[webjarid] + '/' + path;\n" +
-          "    }\n" +
-          "};\n" +
-          "\n" +
-          "var previousRequireCallback;\n" +
-          "if(require && require.callback) {\n" +
-          "    previousRequireCallback = require.callback;\n" +
-          "}" +
-          "var require = {\n" +
-          "    callback: function() {\n" +
-          "        if(previousRequireCallback) previousRequireCallback();" +
-          "        // no-op webjars requirejs plugin loader for backwards compatibility\n" +
-          "        define('webjars', function () {\n" +
-          "            return { load: function (name, req, onload, config) { onload(); } }\n" +
-          "        });\n" +
-          "\n" +
-          "        // all of the webjar configs from their webjars-requirejs.js files\n" +
-          webjarConfigsString.toString +
-          "    }\n" +
-        "}"
+        val javaScript = s"""
+          |var webjars = {
+          |    versions: { ${webjarsVersionsString.toString()}  },
+          |    path: function(webjarid, path) {
+          |        return $webjarUrlPrefix + webjarid + '/' + webjars.versions[webjarid] + '/' + path;
+          |    }
+          |};
+          |
+          |var previousRequireCallback;
+          |if(require && require.callback) {
+          |    previousRequireCallback = require.callback;
+          |}
+          |var require = {
+          |    callback: function() {
+          |        if(previousRequireCallback) previousRequireCallback();
+          |        // no-op webjars requirejs plugin loader for backwards compatibility
+          |        define('webjars', function () {
+          |            return { load: function (name, req, onload, config) { onload(); } }
+          |        });
+          |
+          |        // all of the webjar configs from their webjars-requirejs.js files
+          |        ${webjarConfigsString.toString()}
+          |    }
+          |}
+        """.stripMargin
         requireConfig = Some(javaScript)
         javaScript
   }}
 
   def getWebJarConfig(webjar: WebJar) = {
 
-    val filename = WebJarAssetLocator.WEBJARS_PATH_PREFIX + "/" + webjar.name + "/" + webjar.version + "/" + "webjars-requirejs.js"
+    val filename = s"${WebJarAssetLocator.WEBJARS_PATH_PREFIX}/${webjar.name}/${webjar.version}/webjars-requirejs.js"
     val inputStream = Option(classOf[RequireJS].getClassLoader.getResourceAsStream(filename))
     inputStream.fold(""){inputStream =>
 
-      val config = "// webjar config for " + webjar.name + "\n" +
+      val config = s"// webjar config for ${webjar.name} \n" +
         withSource(Source.fromInputStream(inputStream)) { s =>
             s.getLines().mkString("\n")
         }
